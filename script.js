19213e4a7f0322a9f17191d7c4b0c8babe1b90cf
bankole <bankoleibrahim96@gmail.com>
2021-04-15 16:13:01 +0100

    changes

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const burger = document.querySelector(".burger");
 const nav = document.querySelector(".nav");
+const example = document.querySelector(".logo");
+
+let code = example.innerHTML;
+code = code + "international";
+console.log(code);
 
 burger.addEventListener("click", function () {
   burger.classList.toggle("active");
